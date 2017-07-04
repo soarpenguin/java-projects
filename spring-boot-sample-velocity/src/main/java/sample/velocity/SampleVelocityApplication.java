fe29cd0906dcc46658daac53280c2b6bf11f82dc
soarpenguin <soarpenguin@gmail.com>
2017-07-04 23:29:15 +0800

    update SampleVelocityApplication

--- spring-boot-sample-velocity/src/main/java/sample/velocity/SampleVelocityApplication.java
+++ spring-boot-sample-velocity/src/main/java/sample/velocity/SampleVelocityApplication.java
@@ -16,38 +16,11 @@
 
 package sample.velocity;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.velocity.app.VelocityEngine;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.ui.velocity.VelocityEngineUtils;
 
 @SpringBootApplication
-@Deprecated
-public class SampleVelocityApplication implements CommandLineRunner {
-
-	@Value("${application.message}")
-	private String message;
-
-	@Autowired
-	private VelocityEngine engine;
-
-	@Override
-	public void run(String... args) throws Exception {
-		Map<String, Object> model = new HashMap<String, Object>();
-		model.put("time", new Date());
-		model.put("message", this.message);
-		System.out.println(VelocityEngineUtils.mergeTemplateIntoString(this.engine,
-				"welcome.vm", "UTF-8", model));
-	}
-
+public class SampleVelocityApplication{
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SampleVelocityApplication.class, args);
 	}
